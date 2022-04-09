2ea159198d6a01db29b9530d18a29c549d5ddd3e
Khepu <makris.giwrgos1234@gmail.com>
2022-04-10 01:57:02 +0300

    Feature: Truncation now supports depth argument

--- src/bin/truncate.rs
+++ src/bin/truncate.rs
@@ -1,11 +1,12 @@
 use std::env::args;
 use std::fs::{read_dir, remove_file};
+
 use crate::util::cli::ensure_canonical_directory;
 
 mod util;
 
 /// Deletes all files under the `directory` provided but preserves any directories.
-fn truncate(directory: String) {
+fn truncate(directory: String, depth: u16) {
     let canonical_directory = ensure_canonical_directory(&directory);
 
     read_dir(&canonical_directory)
@@ -20,10 +21,17 @@ fn truncate(directory: String) {
             .path())
         .for_each(|path|
             if path.is_file() {
-                remove_file(path)
+                remove_file(&path)
                     .unwrap_or_else(|error| panic!(
-                        "Could not delete file '{}'",
-                        path.file_name().to_str().unwrap()))
+                        "Could not delete file '{}'! {:?}",
+                        path.file_name().unwrap().to_str().unwrap(),
+                        error))
+            } else if depth > 0 {
+                let nested_directory = path.to_str()
+                    .unwrap_or_else(|| panic!("Could not truncate nested directory!"))
+                    .to_string();
+
+                truncate(nested_directory, depth - 1);
             });
 }
 
@@ -31,5 +39,14 @@ fn main() {
     let directory = args()
         .nth(1)
         .expect("No path provided!");
-    explode(directory)
+
+    let depth = args()
+        .nth(2)
+        .map(|depth_str| depth_str.parse::<u16>()
+            .unwrap_or_else(|error| panic!(
+                "Could not parse argument 'depth'! {:?}",
+                error)))
+        .unwrap_or(0);
+
+    truncate(directory, depth)
 }
